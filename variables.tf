be9defa33ccc9aea7e6cb4392584c93183888796
Ubuntu <ubuntu@ip-172-31-2-213.ap-northeast-2.compute.internal>
2023-05-15 01:03:36 +0000

    commit

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable name {
+  default = "tf-nks"
+}
+
+variable nks_version {
+  default = "1.23"
+}
+
+variable client_ip {
+  default = "YOUR_CLIENT_IP"
+}
+
+variable access_key {
+  default = "i0I4Vi71tycXpGararqZ"
+}
+
+variable secret_key {
+  default = "kGaHFzLQL3WpZtWhRnTfgfQ3T3jeuiSr6ul1Mxde"
+}
+
+variable login_key {
+  type        = string
+  description = "The login key name for the instances."
+  default     = "my-login-key"
+}
